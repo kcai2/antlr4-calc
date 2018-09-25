32ec54e75b5d23d8454e3ba5af05cab4bbeb4c68
kcai2 <kcai2@users.noreply.github.com>
2018-09-25 15:46:27 -0700

    added functionality some aspects still not working
    braces returns null
    scientific?
    complex equations give error
    visit signed?
    everything else seems to work

--- src/main/java/kcai/calculator/visit0r.java
+++ src/main/java/kcai/calculator/visit0r.java
@@ -7,18 +7,32 @@ import java.util.*;
 
 public class visit0r extends calculatorBaseVisitor<Double> {
     
-    Map<String, Double> variable = new HashMap<String, Double>();
+    HashMap<String, Double> variable = new HashMap<String, Double>();
     
     @Override
     public Double visitPrintExpr(calculatorParser.PrintExprContext ctx) {
-        return visitChildren(ctx);
+        //same thing as expressoin in other gramamr?
+//        return visitChildren(ctx);
+        System.out.println("expression called");
+        
+        Double value = visit(ctx.expr());
+        System.out.println(value);
+        return value;
     }
     
     @Override
     public Double visitAssign(calculatorParser.AssignContext ctx) {
-        return visitChildren(ctx);
+        System.out.println("assign variable values");
+        //assign value to variable
+        String key = ctx.VARIABLE().getText();
+        double value = visit(ctx.expr());
+        variable.put(key, value);
+        return value;
     }
     
+    /**
+     * need to do signed..
+     */
     @Override 
     public Double visitSigned(calculatorParser.SignedContext ctx) {
         return visitChildren(ctx);
@@ -26,39 +40,66 @@ public class visit0r extends calculatorBaseVisitor<Double> {
     
     @Override 
     public Double visitAddSub(calculatorParser.AddSubContext ctx) {
-        return 1.0;
+        System.out.println("add called");
+        Double value1 = visit(ctx.expr(0));
+        Double value2 = visit(ctx.expr(1));
+        if (ctx.op.getType() == calculatorParser.MINUS) 
+            return value1 - value2;
+        else
+            return value1 + value2;
+        //return result;
     }
     
     @Override 
     public Double visitMulDiv(calculatorParser.MulDivContext ctx) {
-        return 1.0;
+        System.out.println("mult/div called");
+        Double value1 = visit(ctx.expr(0));
+        Double value2 = visit(ctx.expr(1));
+        if (ctx.op.getType() == calculatorParser.TIMES) 
+            return value1 * value2;
+        else 
+            return value1 / value2;
     }
     
     @Override
     public Double visitPower(calculatorParser.PowerContext ctx) {
-        return 1.0;
+        Double num = visit(ctx.expr(0));
+        Double exp = visit(ctx.expr(1));
+        Double result = Math.pow(num, exp);
+        System.out.println("POWWW " + result);
+        return result;
     }
     
+    /*
+     * not sure if this is correct?
+     * works for now 
+     */
     @Override
     public Double visitAtoms(calculatorParser.AtomsContext ctx) {
-        return visitChildren(ctx);
-    }
-    
-    @Override
-    public Double visitBraces(calculatorParser.BracesContext ctx) {
+        System.out.println("atom called?");
         return visitChildren(ctx);
     }
     
     @Override 
     public Double visitLogarithm(calculatorParser.LogarithmContext ctx) {
-        Double base = visit(ctx.expr(0));
-        Double exp = visit(ctx.expr(1));
-        Double result = Math.log(exp)/Math.log(base);
-        return result;
+        if (ctx.expr().size() > 1) {
+            Double base = visit(ctx.expr(0));
+            Double exp = visit(ctx.expr(1));
+            //Double result = Math.log10(exp); //log base 10 def, but if user specify base ..
+            //https://stackoverflow.com/questions/13831150/logarithm-algorithm - used this for equation
+            Double result = Math.log(exp)/Math.log(base);
+            return result;
+        } else {
+            Double exp = visit(ctx.expr(0));
+            //default base 10 if no more args
+            Double result = Math.log10(exp);
+            return result;
+        }
     }
     
     @Override
     public Double visitNaturalLogarithm(calculatorParser.NaturalLogarithmContext ctx) {
+        //java ln is just log..?
         double result = Math.log(visit(ctx.expr()));
         return result;
     }
@@ -81,12 +122,16 @@ public class visit0r extends calculatorBaseVisitor<Double> {
         return result;
     }
     
+    // acos(pi/2) returns Nan..?
+    // google calculator returns error, so assuming this works for now..
     @Override
     public Double visitCosine(calculatorParser.CosineContext ctx) {
         Double result = Math.cos(visit(ctx.expr()));
         return result;
     }
     
+    // acos(pi/2) returns Nan..?
+    // google calculator returns error, so assuming this works for now..
     @Override
     public Double visitACosine(calculatorParser.ACosineContext ctx) {
         Double result = Math.acos(visit(ctx.expr()));
@@ -107,27 +152,52 @@ public class visit0r extends calculatorBaseVisitor<Double> {
     
     @Override
     public Double visitInteger(calculatorParser.IntegerContext ctx) {
-        return 1.0;
+        System.out.println("INt");
+        return Double.valueOf(ctx.getText());
     }
     
     @Override
     public Double visitDouble(calculatorParser.DoubleContext ctx) {
-        return 1.0;
+        System.out.println("Double");
+        return Double.valueOf(ctx.getText());
     }
     
     @Override
     public Double visitConstantPI(calculatorParser.ConstantPIContext ctx) {
+        System.out.println("3.14159");
         return Math.PI;
     }
     
     @Override 
     public Double visitConstantE(calculatorParser.ConstantEContext ctx) {
+        System.out.println("E1? E2? figure out returning each");
         return Math.E;
     }
     
     @Override
     public Double visitVariable(calculatorParser.VariableContext ctx) {
+        System.out.println("valid id char?");
         return visitChildren(ctx);
+    }
+    
+    /*****************************************************************************************************8
+     * requires fix!!!! returns null when parenthesis are involved...
+     ****************************************************************************************************/
+    @Override
+    public Double visitBraces(calculatorParser.BracesContext ctx) {
+        System.out.println("parenthesis calle?");
+        // returns null...
+        return visitChildren(ctx);
+    }   
+    
+    /***********************************************************************************************
+     * unsure what to do with this
+     **********************************************************************************************/
+    @Override
+    public Double visitScientific(calculatorParser.ScientificContext ctx) {
+        System.out.println("Scientific");
+        
+        return 1.0;
     }
     
     
