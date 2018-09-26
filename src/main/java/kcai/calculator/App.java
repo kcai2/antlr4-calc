e46c6eedf8832c0dc044ff9300dd10caa5e7817c
kcai2 <kcai2@users.noreply.github.com>
2018-09-26 15:58:13 -0700

    done without jline
    jline having trouble importing

--- src/main/java/kcai/calculator/App.java
+++ src/main/java/kcai/calculator/App.java
@@ -1,24 +1,76 @@
 package kcai.calculator;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.FileInputStream;
 
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.reader.EndOfFileException;
+
+// for GUI astree visual
+//import javax.swing.JFrame;
+//import org.antlr.v4.gui.TreeViewer;
+
 
 public class App 
 {
-    public static void main (String[] args) throws IOException {
-        
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
-        calculatorLexer lexer = new calculatorLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        calculatorParser parser = new calculatorParser(tokens);
-        ParseTree ptree = parser.expr();
-        
+    public static void main (String[] args) throws Exception {
         visit0r visit = new visit0r();
-        Double result = visit.visit(ptree);
-        System.out.println(result);
+        LineReader read = LineReaderBuilder.buider().build();
+        String equation = "Enter your math problem or expression: ";
+        while(true) {
+            String line = null;
+            try {
+                line = read.readLine(equation);
+                calculatorParser parser = visit0r.createParser(line);
+                ParseTree ptree = parser.prog();
+                Double result = visit.visit(ptree);
+                if (result != null) System.out.println(result);
+            } catch (UserInterruptException e) {
+                
+            } catch (EndOfFileException e) {
+                return;
+            }
+        }
+        
+//        ANTLRInputStream input = new ANTLRInputStream(System.in);
+//        calculatorLexer lexer = new calculatorLexer(input);
+//        CommonTokenStream tokens = new CommonTokenStream(lexer);
+//        calculatorParser parser = new calculatorParser(tokens);
+//        ParseTree ptree = parser.prog();
+//        
+//        visit0r visit = new visit0r();
+//        Double result = visit.visit(ptree);
+//        System.out.println(result);
+    
+        
+        /***************************************************************************************************
+         //*  Code used to draw out the astree, helped me to visualize where errors were coming from
+         //***************************************************************************************************
+        
+        String expression = "2 + 2 * 5 * 4 / 2";
+        
+        CharStream stream = CharStreams.fromString(expression);
+        calculatorLexer lex = new calculatorLexer(stream);
+        TokenStream tks = new CommonTokenStream(lex);
+        
+        calculatorParser prsr = new calculatorParser(tks);
+        ParseTree tree = prsr.expr();
+        System.out.println("String tree = " + tree.toStringTree());
+
+        JFrame frame = new JFrame("AST for expression: " + expression);
+        TreeViewer treeViewer = new TreeViewer(Arrays.asList(prsr.getRuleNames()), tree);
+        treeViewer.setScale(1.5);
+        frame.add(treeViewer);
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+        //************************************************************************************/
     }
+    
 }
